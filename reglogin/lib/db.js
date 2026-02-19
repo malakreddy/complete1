5a5c9200ea27a3b474927be2a22cee1cf3759b29
Malakareddy K <reddymalak558@gmail.com>
2026-02-19 09:42:38 +0530

    Fix: Update db.js to use DATABASE_URL string directly

--- reglogin/lib/db.js
+++ reglogin/lib/db.js
@@ -1,14 +1,4 @@
 
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-export default pool;
+const pool = mysql.createPool(process.env.DATABASE_URL);
